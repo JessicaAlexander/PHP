2a929b1ed8f328bcb6d122d79fc9fc0b40f24bdb
JessicaAlexander <42972523+JessicaAlexander@users.noreply.github.com>
2019-01-31 13:00:45 -0600

    Add files via upload

---
+++ HelloWorld.php
@@ -0,0 +1,24 @@
+<?php
+# --------------
+# -- Programmer:  [Jessica Alexander]
+# -- Course:      ITSE-1306 (Intro to PHP)
+# -- Instructor:  Cesar "Coach" Marrero
+# -- Assignment:  [Use of echo() commands]
+# -- Description: [Week 3 - Lab]
+# ---------------
+
+
+echo '<!DOCCTYPE HTML>';
+echo '<html lang="en">';
+echo '<head>';
+echo '<meta charset="utf-8">';
+echo '<Hello World>';
+echo '<link rel="stylesheet" href="personal.css" />';
+echo '</head>';
+echo '<body>';
+echo '<h1>Jessica Alexander</h1>';
+echo '<p>Hello World, my name is Jessica Alexander. I am a student at TSTC. I plan on becoming a computer programmer. I live in Carthage Texas.</p>';
+echo '<img src="ME.jpg" alt="A picture of myself" width="750px" height="auto" />';
+echo '</body>';
+echo '</html>';
+?>
